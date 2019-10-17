4a67f5f495565d17050fd498baab2e7c1f1eebc5
Natalia Dobranova <dv_nataly@mail.ru>
2019-10-17 22:24:45 +0300

    Add project files.

---
+++ ConsoleApp2/ModelEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public enum ModelEnum
+    {
+       Jawa = 0,
+       Yamaha = 1,
+       MUSSTaNG = 2,
+       ZONDER = 3,
+       ACXA = 4
+    }
+
+}
---
+++ ConsoleApp2/Motorbike.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Motorbike
+    {
+        private ModelEnum model;
+        private float price;
+        private float volume_tank;
+        private int max_speed;
+        private int Pengine;
+
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.Pengine.ToString());
+            sb.AppendLine(this.max_speed.ToString());
+            sb.AppendLine(this.volume_tank.ToString());
+            sb.AppendLine(this.price.ToString());
+            sb.AppendLine(this.model.ToString());
+
+            return sb.ToString();
+        }
+
+        public Motorbike()  {  }
+
+
+
+        public void Hello(Motorbike motorbike)
+        {
+            Console.WriteLine("Hello! I have a motorbike with next characteristics:" + motorbike);
+        }
+    }
+}
